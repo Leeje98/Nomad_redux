509adf74ff6f925950c82ab35c3b02e9ea8ab89c
lee Jeong Eun <wjd7dms7@naver.com>
2022-11-28 16:24:16 +0900

    #1.2 리덕스 사용법 익히기

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
+import { createStore } from "redux";
+
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const numder = document.querySelector("span");
 
-let count = 0;
-
-numder.innerText = count;
-
-const updateText = () => {
-  numder.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1
+  } else if (action.type === "MINUS") {
+    return count - 1
+  } else {
+    return count;
+  }
 };
 
-const handleAdd = () => {
-  console.log("add");
-  count = count + 1
-  updateText()
-};
+const countStore = createStore(countModifier)
+    // store : 나의 data를 저장하는 곳
 
-const handleMinus = () => {
-  count = count - 1
-  updateText()
-};
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
+
+
