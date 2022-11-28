bc2fe434de8a6b64daf6da54e65c3994225a2bc6
lee Jeong Eun <wjd7dms7@naver.com>
2022-11-28 17:54:35 +0900

    #1.4 리덕스 효율적으로 쓰기(스트링보다 const로 선언해서 쓰기 / if문 보다 swich문 쓰기)

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 import { createStore } from "redux";
 
-
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const numder = document.querySelector("span");
 
+numder.innerText = 0
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return count + 1
-  } else if (action.type === "MINUS") {
-    return count - 1
-  } else {
-    return count;
+  // console.log(count, action);
+  // if (action.type === "ADD") {
+  //   return count + 1;
+  // } else if (action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }    // if문보다 switch case문을 쓰는게 일반적이다
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
-const countStore = createStore(countModifier)
-    // store : 나의 data를 저장하는 곳
+const countStore = createStore(countModifier);
+// store : 나의 data를 저장하는 곳
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  numder.innerText = countStore.getState();
+}
 
-console.log(countStore.getState());
+countStore.subscribe(onChange)
 
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD})
+                     // action은 {오브젝트}여야한다. 스트링이 될 수 없다
+};
 
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS})
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
